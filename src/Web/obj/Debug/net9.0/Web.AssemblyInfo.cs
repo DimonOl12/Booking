72b64603d0dd8020b9b0b56219350f0d4e3bd0ba
DimonOl12 <dimon52266552@gmail.com>
2026-03-10 03:31:38 +0200

    beta_reg_ac

--- src/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ src/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3ed471990b12ecec612d9ee45428d06d279e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11685fb8fbce9b5f8409fade6c93cf4acc662373")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
